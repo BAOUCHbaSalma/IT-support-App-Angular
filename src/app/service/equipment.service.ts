6d2c0d50fecf7e6bdd0d76845d489aa42988a0b5
BAOUCHbaSalma <baouch.salma0@gmail.com>
2024-08-05 15:18:09 +0100

    j'ai ajoutee les methodes dans service

---
+++ src/app/service/equipment.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Equipment } from '../model/it-support';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EquipmentService {
+  urlApiUser="http://localhost:8080/user"
+  urlApiAdmin="http://localhost:8080/admin"
+  urlApiTechnician="http://localhost:8080/technician"
+  urlApi="http://localhost:8080"
+
+  constructor(private http:HttpClient) { }
+
+  public addEquipement(equipment:Equipment){
+    return this.http.post(`${this.urlApiAdmin}/equipement`,equipment)
+  }
+  public showEquipements(){
+    return this.http.get(`${this.urlApiAdmin}/equipments`)
+  }
+}
