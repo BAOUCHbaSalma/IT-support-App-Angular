038d0846d4ab2b016aebb175e16acabc8f913aa6
BAOUCHbaSalma <baouch.salma0@gmail.com>
2024-08-08 15:03:27 +0100

    j'ai cree le service de failure avec leurs methodes

---
+++ src/app/service/failure.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Failure } from '../model/it-support';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FailureService {
+
+  urlApiUser="http://localhost:8080/user"
+  urlApiAdmin="http://localhost:8080/admin"
+  urlApiTechnician="http://localhost:8080/technician"
+  urlApi="http://localhost:8080"
+
+  constructor(private http:HttpClient) { }
+
+  public addFailure(failure :Failure){
+    return this.http.post(`${this.urlApiAdmin}/failure`,failure)
+  }
+  public deleteFailure(idFailure:number){
+    return this.http.delete(`${this.urlApiAdmin}/failure/${idFailure}`)
+  }
+  public updateFailure(idFailure:number,failure:Failure){
+    return this.http.put(`${this.urlApiAdmin}/failure/${idFailure}`,failure)
+  }
+}
